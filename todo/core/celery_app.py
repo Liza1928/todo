3fa6c5db07cf94c390225fd34bafdc5460dd9c40
Liza1928 <lizochka.dostoevskaya@mail.ru>
2021-05-30 16:29:45 +0300

    Добавлены тестовые задачи для celery

--- todo/core/celery_app.py
+++ todo/core/celery_app.py
@@ -1,5 +1,12 @@
 from celery import Celery
 
-celery_app = Celery("worker", broker="amqp://guest@queue//")
 
-celery_app.conf.task_routes = {"app.worker.test_celery": "main-queue"}
+celery_app = Celery(
+        "worker",
+        backend="redis://:password123@redis:6379/0",
+        broker="amqp://user:bitnami@rabbitmq:5672//"
+    )
+
+celery_app.conf.task_routes = {
+        "todo.core.celery_worker.test": "test-queue"}
+celery_app.conf.update(task_track_started=True)
